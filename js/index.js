d44af14971568f9ad063c423efc241f7df65dfb3
Abdullah13130 <96126010+Abdullah13130@users.noreply.github.com>
2023-08-21 13:09:58 +0600

    summer sale done

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ function handleClickCard(target) {
     }
 
     // apply button
-    
+
     const btnApply = document.getElementById('total').innerText;
     const disableApplyBtn = document.getElementById('disable-apply-btn');
     if (btnApply >= 200) {
@@ -36,3 +36,46 @@ function handleClickCard(target) {
 }
 
 
+// promo code
+
+document.getElementById('disable-apply-btn').addEventListener('click', function () {
+    const promoId = document.getElementById('promo-id').value
+    if (promoId !== 'SELL200') {
+        alert('Promo id is not valid')
+        return;
+    }
+    const discountPrice = document.getElementById('total');
+    const totalPrice = discountPrice.innerText
+    const price = totalPrice * 0.2;
+    const discount = document.getElementById('discount');
+    discount.innerText = price.toFixed(2);
+    const grandTotal = document.getElementById('grand-total');
+    grandTotal.innerText = (totalPrice - price).toFixed(2);
+})
+
+// go to home
+
+document.getElementById('home-btn').addEventListener('click', function () {
+
+    const discountPrice = document.getElementById('total');
+    discountPrice.innerText = 0;
+
+
+    const discount = document.getElementById('discount');
+    discount.innerText = 0;
+
+    const grandTotal = document.getElementById('grand-total');
+    grandTotal.innerText = 0;
+
+    const li = document.getElementById('selected-item');
+    li.innerText = '';
+
+    document.getElementById('promo-id').value = ''
+
+    document.getElementById('disable-apply-btn').setAttribute('disabled', true);
+
+    document.getElementById('make-purchase').setAttribute('disabled', true);
+
+    total = 0;
+
+})
